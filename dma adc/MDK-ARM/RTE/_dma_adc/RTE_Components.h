6a504a6502f2bc5d3b252c6d5609a07999260329
Tuan Anh Nguyen Chi <ainha475@gmail.com>
2022-11-17 18:06:07 +0700

    first commit

---
+++ dma adc/MDK-ARM/RTE/_dma_adc/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'dma adc' 
+ * Target:  'dma adc' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ dma adc/MDK-ARM/dma adc/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+dma adc\startup_stm32f103xb.o: startup_stm32f103xb.s
